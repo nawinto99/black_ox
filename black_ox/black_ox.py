658e13420f9600b305ffcc39ec179994d9627e29
Naveen Thurimerla <79765453+nawinto99@users.noreply.github.com>
2021-03-23 23:45:46 +0530

    add db conf

--- black_ox/black_ox.py
+++ black_ox/black_ox.py
@@ -1,1 +1,10 @@
-"""Main module."""
+import yaml
+
+conf = yaml.load(open('conf/application.yml'), Loader=yaml.FullLoader)
+engine = conf['db']['engine']
+server = conf['db']['server']
+database = conf['db']['database']
+username = conf['db']['username']
+password = conf['db']['password']
+
+print (engine)
